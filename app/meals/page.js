f1b3ffdd75341aacbc3b135659bda4eed32a9b3f
ddangmul <j.ddangmul@gmail.com>
2025-01-25 20:04:43 +0900

    컴포넌트 아웃소싱 & Suspense를 이용해 meals 정보 로딩 시 fallback 출력 및 헤더 항상 출력

--- app/meals/page.js
+++ app/meals/page.js
@@ -3,16 +3,21 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import getMeals from "@/lib/meals";
+import { Suspense } from "react";
 
-export default async function MealsPage() {
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   // Next앱은 백엔드와 프론트엔드가 결합된 풀스택 앱이므로 백엔드를 분리시킬 필요가 없다.
   // 기본적으로 모두 서버 컴포넌트이기 때문에 이미 백엔드 서버를 가지고 있다.
   // ==> useEffect, fetch 사용할 필요 x
   // useEffect(() => {
   //   fetch()
   // }, [])
-
-  const meals = await getMeals();
 
   return (
     <>
@@ -29,7 +34,11 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
