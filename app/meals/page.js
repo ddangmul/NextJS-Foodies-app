c58d246f3c3bad5fc54b8f5b7a45042329971e32
ddangmul <j.ddangmul@gmail.com>
2025-01-25 16:00:07 +0900

    NextJS db작업 및 비동기 서버 컴포넌트 기능으로 데이터 불러오기 & 로딩 페이지 loading.js 추가

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,8 +2,18 @@ import Link from "next/link";
 
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import getMeals from "@/lib/meals";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  // Next앱은 백엔드와 프론트엔드가 결합된 풀스택 앱이므로 백엔드를 분리시킬 필요가 없다.
+  // 기본적으로 모두 서버 컴포넌트이기 때문에 이미 백엔드 서버를 가지고 있다.
+  // ==> useEffect, fetch 사용할 필요 x
+  // useEffect(() => {
+  //   fetch()
+  // }, [])
+
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -19,7 +29,7 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
